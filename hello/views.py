194e00102d6a4bab3736373203c7a94fc3c56d95
inaz2 <inaz2@users.noreply.github.com>
2016-06-02 15:52:11 +0900

    initial

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+
+import re
+from datetime import timedelta
+import json
+import tweepy
 
 from .models import Greeting
 
-# Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
 
-def db(request):
+def tweets(request):
+    screen_name = request.GET.get('screen_name')
+    if not re.search(r'^\w+$', screen_name):
+        return HttpResponseBadRequest()
 
-    greeting = Greeting()
-    greeting.save()
+    consumer_key="A6fiN33tLablelq9pfgbILvfT"
+    consumer_secret="MlfSrZem0iSg1Ku7L2nadF01XzwgPlWG5090Ww9Y1lPrWHkPMu"
+    access_token="933582354-t0VlfS295q3kTvCuDvz0OTXTJuk8Lax5vb73vPDN"
+    access_token_secret="7l1Hq2vPWwFpAmEiOReQ06zJa0AoZRqKTUXRYWvCy6dSU"
 
-    greetings = Greeting.objects.all()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
+    auth.set_access_token(access_token, access_token_secret)
 
-    return render(request, 'db.html', {'greetings': greetings})
+    api = tweepy.API(auth)
+    tweets = api.user_timeline(screen_name=screen_name, count=200)
+    stat = [[0 for j in xrange(24)] for i in xrange(7)]
+    for tweet in tweets:
+        ts = tweet.created_at
+        ts += timedelta(hours=+9)
+        stat[ts.weekday()][ts.hour] += 1
 
+    data = []
+    for day, v in enumerate(stat):
+        for hour, count in enumerate(v):
+            data.append({'day': day+1, 'hour': hour, 'value': count})
+
+    return HttpResponse(json.dumps(data), content_type='application/json')
